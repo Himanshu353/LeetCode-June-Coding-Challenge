fea6ad6160c6a8a2565adc33db382312f2d79993
Himanshu Kakkar <himanshukakkar353@gmail.com>
2020-06-14 23:32:32 +0530

    Create Cheapest Flights Within K Stops.cpp

---
+++ Cheapest Flights Within K Stops.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minCost=INT_MAX;
+    int visited=0;
+    void fun(int src,int dst,unordered_map<int,vector<pair<int,int>>> &graph,int k,int cost)
+    {
+        if(src==dst)
+        {
+            minCost=min(minCost,cost);
+            visited=1;
+            return;
+        }
+        if(cost>=minCost || k<0)
+        {
+            return;
+        }
+        for(pair<int,int> p: graph[src])
+        {
+            fun(p.first, dst, graph, k-1, cost+p.second);
+        }            
+    }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K)
+    {
+        unordered_map<int,vector<pair<int,int>>>graph;
+        for(int i=0;i<flights.size();i++)
+        {
+            graph[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+        }
+        int cost=0;
+        fun(src,dst,graph,K,cost);
+        if(!visited)
+        {
+            return -1;
+        }
+        return minCost;
+    }
+};
